d49ba17d0fd730c7ca22b72de5227d6b396505d5
viicruz <victtorrhugo100@gmail.com>
2026-09-16 15:31:41 -0300

    Enhance frame rendering in index.ts by adding terminal control sequences for alternate screen, cursor hiding, and line wrapping. Improve frame rate management with precise sleep timing based on elapsed frame processing time.

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,32 @@ const width = 100;
 
 const FRAME_NS = 1_000_000_000 / 30;
 
+process.stdout.write(
+  "\x1b[?1049h" + // alternate screen (não mistura com o scrollback)
+  "\x1b[?25l"   + // esconde o cursor
+  "\x1b[?7l"      // desliga wrap (evita scroll se a linha tiver 100 cols)
+);
+
 //loop and print each frame
 while (true) {
+
+  // process.stdout.write("\x1Bc");
+  // process.stdout.write("\x1b[H");
+  //t0 is the start time of the frame
   const t0 = Bun.nanoseconds();
+  //run the frame
   const framebuffer = emulator.runFrame();
+  //render the frame and define the width, and type of rendering
   const frame = renderFramebuffer(framebuffer, "ansi-half", width);
+
+  //concatenate the frame with the clear screen command and break line
   process.stdout.write("\x1b[H" + frame);
+
+  //elapsed is the time taken to run the frame
   const elapsed = Bun.nanoseconds() - t0;
+
+  //sleepTime is the time to sleep to maintain the frame rate
   const sleepTime = Math.max(0, (FRAME_NS - elapsed) / 1e6);
   await Bun.sleep(sleepTime);
+  // process.stdout.write("\x1b[?25h\x1b[?1049l");
 }
